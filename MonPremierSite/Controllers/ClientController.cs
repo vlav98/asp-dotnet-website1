41f838867e2668a70ed5428433e4e363810fa4c1
Valentine <valentinelav98@gmail.com>
2020-08-07 17:54:37 +0200

    :sparkles: Adding Client part MVC

---
+++ MonPremierSite/Controllers/ClientController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPremierSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonPremierSite.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListeClients()
+        {
+            Clients clients = new Clients();
+            ViewData["Clients"] = clients.ObtenirListeClients();
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            /*if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Client
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);*/
+            Clients clients = new Clients();
+            Client client = clients.ObtenirListeClients().FirstOrDefault(c => c.Id == id);
+            if (client != null)
+            {
+                ViewData.Add("Id", client.Id);
+                ViewData.Add(new KeyValuePair<string, object>("FirstName", client.FirstName));
+                ViewData.Add(new KeyValuePair<string, object>("LastName", client.LastName));
+                ViewData.Add(new KeyValuePair<string, object>("Username", client.Username));
+                ViewData.Add(new KeyValuePair<string, object>("Email", client.Email));                
+                return View("Detail");
+            }
+            return View("NotFound");
+        }
+
+        private IActionResult NotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET: Clients/Delete/5
+        /*public async Task<IActionResult> Delete(string id)
+        {
+            *//*if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Client
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);*//*
+        }*/
+
+        // POST: Clients/Delete/5
+        /*[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            *//*var client = await _context.Client.FindAsync(id);
+            _context.Client.Remove(client);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));*//*
+        }*/
+    }
+}
---
+++ MonPremierSite/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonPremierSite.Models
+{
+    public class Client
+    {
+        public int Id{ get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username{ get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Clients
+    {
+        public List<Client> ObtenirListeClients()
+        {
+            return new List<Client>
+            {
+                new Client { Id = 1, FirstName = "Nicolas", LastName = "N", Username = "nicolasn", Email = "nicolas@gmail.com", Password = "abcdef" },
+                new Client { Id = 2, FirstName = "Delphine", LastName = "N", Username = "delphinen", Email = "delphine@gmail.com", Password = "abcdef" },
+                new Client { Id = 3, FirstName = "Jérémie", LastName = "N", Username = "jeremien", Email = "jeremie@gmail.com", Password = "abcdef" },
+                new Client { Id = 4, FirstName = "Timéo", LastName = "N", Username = "timeon", Email = "timeo@gmail.com", Password = "abcdef" }
+            };
+        }
+    }
+}
